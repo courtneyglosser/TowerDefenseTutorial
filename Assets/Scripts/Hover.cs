9baf5c7d76b261d1fea86e70a07c5337ea8685f9
Courtney Glosser <courtneyglosser@gmail.com>
2021-03-27 15:31:14 -0400

    Escape button clears selected tower from pointer.

--- Assets/Scripts/Hover.cs
+++ Assets/Scripts/Hover.cs
@@ -20,8 +20,11 @@ public class Hover : Singleton<Hover>
 
     private void FollowMouse()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(pos.x, pos.y, 0);
+        if (spriteRenderer.enabled)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(pos.x, pos.y, 0);
+        }
     }
 
     public void Activate(Sprite sprite)
@@ -33,5 +36,6 @@ public class Hover : Singleton<Hover>
     public void Deactivate()
     {
         spriteRenderer.enabled = false;
+        GameManager.Instance.ClickedBtn = null;
     }
 }
